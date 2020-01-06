618eb6bf2ef9132bb5fc34c1a2b68cf6c8a900d2
wise-integer-6834 <ghaxhaj@fordham.edu>
2020-01-06 05:11:10 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,4 +1,4 @@
-require "YAML"
+require "yaml"
 
 def load_library(file_path)
   emoticons = YAML.load_file(file_path)
@@ -16,10 +16,12 @@ def load_library(file_path)
   new_hash
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(file_path, emoticon)
+  result = load_library(file_path)['get_emoticon'][emoticon]
+  result ? result : "Sorry, that emoticon was not found"
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning(file_path, emoticon)
+  result = load_library(file_path)['get_meaning'][emoticon]
+  result ? result : "Sorry, that emoticon was not found"
 end
